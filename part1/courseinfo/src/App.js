c75fb9b3ef48e4c4b8095b8dce81a5cf576cf0da
Charles Low <charles.low@pm.me>
2022-12-03 20:48:49 -0500

    Part 1.2 - courseinfo step 2 completed

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -16,6 +16,15 @@ const App = () => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <p>
+    {props.part} {props.exercises}
+    </p>
+  )
+}
+
+
 const Header = (props) => {
   return (
     <h1>{props.course}</h1>
@@ -24,17 +33,11 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
-    <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
-    </>
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
   )
 }
 
@@ -43,5 +46,6 @@ const Total = (props) => {
     <p>Number of exercises {props.total}</p>
   )
 }
+
 
 export default App
